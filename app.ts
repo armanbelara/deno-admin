a376fcc56bd7b6b7ff467214805ca698c4c6b827
armanbelara <arman@paymongo.com>
2022-01-11 16:11:45 +0800

    Dependency downgraded for deno.v1.6.0

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Application } from 'https://deno.land/x/oak@v10.1.0/mod.ts';
+import { Application } from 'https://deno.land/x/oak@v6.4.1/mod.ts';
 import router from './routes.ts';
 
 const app = new Application();
--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'https://deno.land/x/oak@v10.1.0/mod.ts';
+import { Router } from 'https://deno.land/x/oak@v6.4.1/mod.ts';
 
 const router = new Router();
 
